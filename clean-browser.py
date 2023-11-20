65ec2857b1a0e2c836b9252cbe67c31545694629
MauBorre <122322896+MauBorre@users.noreply.github.com>
2023-11-20 14:11:56 -0300

    name the site. multiple urls allowed

--- clean-browser.py
+++ clean-browser.py
@@ -2,46 +2,72 @@ import os
 from threading import Thread
 import subprocess
 import sys
+import argparse
 
 """
 run:
-python clean-browser.py *url*
+python clean-browser.py $url
+or
+python clean-browser.py github twitch <- NEW name the site. Wish it was more flexible tho
+or
+python clean-browser.py github soundcloud.com <- NEW combine urls with sites
 
 """
 # TODO: integrate an argument parse option so we could call e.g. -chrome -edge, etc...
-url = sys.argv[1] if sys.argv[1].startswith("http") else "https://" + sys.argv[1]
 
-def find_browser():
-    # TODO: make this a dict so we can choose the preferred browser
-    paths = [
-        "C:\Program Files\Google\Chrome\Application\chrome.exe",
-        "C:\Program Files\BraveSoftware\Brave-Browser\Application\\brave.exe",
-        "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
-        "C:\Program Files\Microsoft\Edge\Application\msedge.exe",
-    ]
-    for path in paths:
-        if os.path.exists(path):
-            return path
-    return None
+args = sys.argv
 
-def get_browser_command():
-    flags = [
-        browser_path,
-        f"--user-data-dir=",
-        "--new-window",
-        "--no-first-run",
-    ]
+sites_dict = {
+    "github": "https://github.com",
+    "twitch": "https://twitch.tv",
+}
 
-    flags.extend([f"--app={url}"])
+for argument in args[1:]:
 
-    return flags
+    def match_or_literal(argument):
+        # matches sites_dict key? return sites_dict value
+        if argument in sites_dict.keys():
+            return sites_dict[argument]
+        # its an incomplete url so...
+        else:
+            return argument if argument.startswith("http") else "https://" + argument
 
-browser_path = find_browser()
-browser_command = get_browser_command()
 
-def start_browser():
-    subprocess.run(browser_command)
 
-browser_thread = Thread(target=start_browser)
+    url = match_or_literal(argument)
 
-browser_thread.start()
+    def find_browser():
+        # TODO: make this a dict so we can choose the preferred browser
+        paths = [
+            "C:\Program Files\Google\Chrome\Application\chrome.exe",
+            "C:\Program Files\BraveSoftware\Brave-Browser\Application\\brave.exe",
+            "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
+            "C:\Program Files\Microsoft\Edge\Application\msedge.exe",
+        ]
+        for path in paths:
+            if os.path.exists(path):
+                return path
+        return None
+
+    def get_browser_command():
+        flags = [
+            browser_path,
+            f"--user-data-dir=",
+            "--new-window",
+            "--no-first-run",
+        ]
+
+        flags.extend([f"--app={url}"])
+
+        return flags
+
+    browser_path = find_browser()
+    browser_command = get_browser_command()
+
+    def start_browser():
+        subprocess.run(browser_command)
+
+    print(f"opening url: {url}")
+    browser_thread = Thread(target=start_browser)
+
+    browser_thread.start()
